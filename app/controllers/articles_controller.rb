4e37bbe56201a5f7f602d3b1e17eb4a5a58af83b
wilfriedmuffins <wilfried.moffen@live.fr>
2020-12-14 17:21:09 +0100

    article edit ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,20 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article bel et bien modifier"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
     private
 
     def article_params
