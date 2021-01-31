5c6301fade1aa28299f3f2c8f4e2cedb8d1ad06c
wilfriedmuffins <wilfried.moffen@live.fr>
2021-01-31 20:43:23 +0100

    Revert " user validate"
    This reverts commit 009a4d228fb383f3f1720bb4eca9e6c0fbf38921.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
 class User < ActiveRecord::Base
-    #validation pour le taille mini et max + elment unique pour le username
     
-    validates :username, presence: true, 
-                        uniqueness: { case_sensitive: false }, 
-                        length: {minimum: 3, maximum: 25}
-
-    VALID_EMAIL_REGEX = /.+\@.+\..+/i
-
-    validates :email, presence: true, 
-                        uniqueness: { case_sensitive: false }, 
-                        format: { with: VALID_EMAIL_REGEX }
 end
