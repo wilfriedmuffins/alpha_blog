539c0b0c4e1555ae3dc9d4f375f4720d78512ec8
wilfriedmuffins <wilfried.moffen@live.fr>
2021-02-13 15:32:48 +0100

    form session added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def new
+        
+    end
+
+    def create
+        debugger
+        render "new"
+    end
+
+    def destroy #decoonection=destroy la session
+        
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "users#new"
   resources :users, except: (:new) #creation des liens
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
